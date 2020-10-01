1bba71a622378c805b731b45e30650317580a6f4
Shiva Janssens <shiva.janssens@solita.fi>
2020-10-01 16:32:15 +0200

    hupa

--- backend.js
+++ backend.js
@@ -1,1 +1,7 @@
-document.getElementById("bjorn").innerHTML = "Shiva";
+var xhr = new XMLHttpRequest();
+var url = "http://127.0.0.1:8000/nlp/returnOrganisations/";
+xhr.open("POST", url, true);
+xhr.setRequestHeader("Content-Type", "application/json");
+
+var data = JSON.stringify({"url": "https://hehe.be", "text": "My name is Shiva and I work at Solita Belgium."});
+xhr.send(data);
