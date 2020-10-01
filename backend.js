db6933e4cc977a6b451d2d08a4e0171c3b88a5a2
Shiva Janssens <shiva.janssens@solita.fi>
2020-10-01 17:13:54 +0200

    this is not python pls use ;

--- backend.js
+++ backend.js
@@ -2,6 +2,9 @@ function submitRequest() {
     var urlInput = document.getElementById('url').innerHTML;
     var text = document.getElementById('text').innerHTML;
 
+    console.log(urlInput)
+    console.log(text)
+
     var xhr = new XMLHttpRequest();
     var url = "http://127.0.0.1:8000/nlp/returnOrganisations/";
     xhr.open("POST", url, true);
@@ -10,12 +13,15 @@ function submitRequest() {
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             var json = JSON.parse(xhr.responseText);
+            console.log(json)
             console.log(json.url + ", " + json.text + ", " + json.companies);
         }
     };
 
     var data = JSON.stringify({"url": urlInput, "text": text});
     xhr.send(data);
+
+    console.log("request send")
 }
 
 
