c6d34272d26948b446de37519e85356cfc1fd456
Shiva Janssens <shiva.janssens@solita.fi>
2020-10-01 16:42:08 +0200

    connection to backend live

--- backend.js
+++ backend.js
@@ -5,3 +5,10 @@ xhr.setRequestHeader("Content-Type", "application/json");
 
 var data = JSON.stringify({"url": "https://hehe.be", "text": "My name is Shiva and I work at Solita Belgium."});
 xhr.send(data);
+
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        var json = JSON.parse(xhr.responseText);
+        console.log(json.companies + ", " + json.persons);
+    }
+};
